3dcaa7c290c869eaf3fb00dd4b48bcd8545affc4
nafanya_pc <nafanya_pc@example.com>
2025-12-11 01:02:27 +0300

    начата работа с логером

--- app/bot.py
+++ app/bot.py
@@ -1,13 +1,39 @@
 from aiogram import Bot, Dispatcher
 
 from app.config import config
-from app.logging import logger
+# from app.logging import logger
 from app.handlers import router
+
+
+import typing
+from aiogram import BaseMiddleware, types
+
+from loguru import logger
+
+
+def get_user_info(user: types.User) -> str:
+    return f"{user.id} | {user.full_name} | @{user.username if user.username else 'Отсутствует'}"
+
+
+class LogActionMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: typing.Callable[[types.TelegramObject, typing.Dict[str, typing.Any]], typing.Awaitable[typing.Any]],
+        event: types.TelegramObject,
+        data: typing.Dict[str, typing.Any],
+    ) -> typing.Any:
+        if isinstance(event, types.Message):
+            logger.info(f"{get_user_info(event.from_user)} - сообщение: {event.text}")
+        elif isinstance(event, types.CallbackQuery):
+            logger.info(f"{get_user_info(event.from_user)} - action: {event.data}")
+
+        return await handler(event, data)
 
 
 async def main():
     bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher()
+    # setup_middlewares(dispatcher)
 
     dp.include_router(router)
 
